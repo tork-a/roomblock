342f8006fc90a7925475d13790d53805b873b5e6
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2017-03-29 14:33:49 +0000

    Modify device install scripts

--- roomblock_bringup/scripts/create_udev_rules.sh
+++ roomblock_bringup/scripts/create_udev_rules.sh
@@ -5,6 +5,12 @@ echo "roomba usb connection as /dev/roomba, check it using the command : ls -l /
 echo "start copy roomba.rules to  /etc/udev/rules.d/"
 echo "`rospack find roomblock_bringup`/scripts/roomba.rules"
 sudo cp `rospack find roomblock_bringup`/scripts/roomba.rules  /etc/udev/rules.d
+
+echo "remap the device serial port(ttyUSBX) to  rplidar"
+echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
+echo "start copy rplidar.rules to  /etc/udev/rules.d/"
+echo "`rospack find roomblock_bringup`/scripts/rplidar.rules"
+sudo cp `rospack find roomblock_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
--- roomblock_bringup/scripts/delete_udev_rules.sh
+++ roomblock_bringup/scripts/delete_udev_rules.sh
@@ -3,6 +3,9 @@
 echo "delete remap the device serial port(ttyUSBX) to roomba"
 echo "sudo rm /etc/udev/rules.d/roomba.rules"
 sudo rm /etc/udev/rules.d/roomba.rules
+echo "delete remap the device serial port(ttyUSBX) to  rplidar"
+echo "sudo rm   /etc/udev/rules.d/rplidar.rules"
+sudo rm   /etc/udev/rules.d/rplidar.rules
 echo " "
 echo "Restarting udev"
 echo ""
